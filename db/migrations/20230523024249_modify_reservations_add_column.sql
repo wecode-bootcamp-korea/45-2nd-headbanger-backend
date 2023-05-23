5f1f77a3d777eea0af7ddb3160425f2da6a64f61
ji eun lee <jieunlee@jiui-MacBookAir.local>
2023-05-23 14:47:11 +0900

    [MODIFY]: modify change table name

--- db/migrations/20230523024249_modify_reservations_add_column.sql
+++ db/migrations/20230523024249_modify_reservations_add_column.sql
@@ -1,7 +1,7 @@
 -- migrate:up
-ALTER TABLE reservation ADD COLUMN total_price DECIMAL(10,2) NOT NULL AFTER end_date;
-ALTER TABLE reservation ADD COLUMN selected_zone VARCHAR NOT NULL AFTER total_price;
-ALTER TABLE reservation ADD COLUMN total_members INT NOT NULL AFTER camping_zone;
+ALTER TABLE reservations ADD COLUMN total_price DECIMAL(10,2) NOT NULL AFTER end_date;
+ALTER TABLE reservations ADD COLUMN selected_zone VARCHAR(50) NOT NULL AFTER total_price;
+ALTER TABLE reservations ADD COLUMN total_members INT NOT NULL AFTER selected_zone;
 ALTER TABLE camps MODIFY COLUMN price DECIMAL(10,2);
 ALTER TABLE camps MODIFY COLUMN latitude VARCHAR(30);
 ALTER TABLE camps MODIFY COLUMN longitude VARCHAR(30); 
